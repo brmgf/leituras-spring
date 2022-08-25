00e5103b871f4adfac7371078b925057ee714880
milene <milenefaria33@gmail.com>
2022-08-24 23:06:31 -0300

    project start

---
+++ src/test/java/com/brmgf/readings/ReadingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brmgf.readings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
